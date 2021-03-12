b5b9e4ae9d64faab96618aecac9eabd31c8db55b
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-03-12 19:32:31 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,7 @@
 def using_while
   levitation_force = 6
 	while levitation_force < 10
-  puts out the phrase "Wingardium Leviosa" while our levitation force is less than 10
+  puts out the phrase "Wingardium Leviosa"
   levitation_force += 1
   end
 end
